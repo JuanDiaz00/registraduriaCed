b99ac5c5ba22b56c313422138d067f5ebee39bfe
JuanDiaz <juane.diazt@konradlorenz.edu.co>
2022-11-20 16:36:04 -0500

    Se edita index.php
    Con respecto a nuestro idioma

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -29,7 +29,7 @@
 						<div class="login-wrap p-4 p-md-5">
 			      	<div class="d-flex">
 			      		<div class="w-100">
-			      			<h3 class="mb-4">Sign In</h3>
+			      			<h3 class="mb-4">Ingresar</h3>
 			      		</div>
 								<div class="w-100">
 									<p class="social-media d-flex justify-content-end">
@@ -41,29 +41,29 @@
 							<form action="#" class="signin-form">
 			      		<div class="form-group mt-3">
 			      			<input type="text" class="form-control" required>
-			      			<label class="form-control-placeholder" for="username">Username</label>
+			      			<label class="form-control-placeholder" for="username">Usuario</label>
 			      		</div>
 		            <div class="form-group">
 		              <input id="password-field" type="password" class="form-control" required>
-		              <label class="form-control-placeholder" for="password">Password</label>
+		              <label class="form-control-placeholder" for="password">Contraseña</label>
 		              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
 		            </div>
 		            <div class="form-group">
-		            	<button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
+		            	<button type="submit" class="form-control btn btn-primary rounded submit px-3">Registrarse</button>
 		            </div>
 		            <div class="form-group d-md-flex">
 		            	<div class="w-50 text-left">
-			            	<label class="checkbox-wrap checkbox-primary mb-0">Remember Me
+			            	<label class="checkbox-wrap checkbox-primary mb-0">Rercuerdame
 									  <input type="checkbox" checked>
 									  <span class="checkmark"></span>
 										</label>
 									</div>
 									<div class="w-50 text-md-right">
-										<a href="#">Forgot Password</a>
+										<a href="#">Olvidaste tu contraseña?</a>
 									</div>
 		            </div>
 		          </form>
-		          <p class="text-center">Not a member? <a data-toggle="tab" href="#signup">Sign Up</a></p>
+		          <p class="text-center">¿No estás registrado?<a data-toggle="tab" href="#signup">Registrate</a></p>
 		        </div>
 		      </div>
 				</div>
