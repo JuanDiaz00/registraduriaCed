d96c84a7e63909fd1efbbc26394d8e9cd922d468
JuanDiaz <juane.diazt@konradlorenz.edu.co>
2022-11-21 13:55:41 -0500

    Se instala FPDF para poder hacer informes PDF
    Además se agrega un pequeño código para hacer PDF basico.

--- views/paginas/admin/admin.php
+++ views/paginas/admin/admin.php
@@ -1,3 +1,16 @@
 <?php
 #Acá voy a hacer lo de generar reporte, no tocar.
+// include class
+require('fpdf/fpdf.php');
+
+// create document
+$pdf = new FPDF();
+$pdf->AddPage();
+
+// add text
+$pdf->SetFont('Arial', '', 12);
+$pdf->Cell(0, 10, 'Generar archivos PDF con PHP', 0, 1);
+
+// output file
+$pdf->Output('', 'basic.pdf');
 ?>
