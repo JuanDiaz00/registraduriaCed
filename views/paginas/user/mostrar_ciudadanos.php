99154e947a739c942eb153f78670795c4329989a
cristin corso <cristianc427@gmail.com>
2022-11-22 18:13:00 -0500

    kk

--- views/paginas/user/mostrar_ciudadanos.php
+++ views/paginas/user/mostrar_ciudadanos.php
@@ -9,34 +9,44 @@ if (!defined('CONFIG_PATH')) {
     define('CONFIG_PATH', '../../../config/');
 }
 
-require_once(CONTROLLER_PATH ."ciudadanos.php");
+require_once(CONTROLLER_PATH . "ciudadanos.php");
 ?>
-<!doctype html>
-<html lang="Es-es">
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <title>Ingreso</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <link rel="stylesheet" href="../estilos/styles.css">
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../estilos/bootstrap.min.css">
+    <link rel="stylesheet" href="../../estilos/registrar_ciudadano.css">
+    <link rel="stylesheet" href="../../estilos/menu.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <title>Administración ciudadano</title>
 </head>
 
 <body>
+    <header class="header">
+        <div class="logo">
+            <img src="../asset/logo.png" alt="Logo de la marca">
+        </div>
+        <nav>
+            <ul class="nav-links">
+                <li><a href="#">Registrar ciudadano</a></li>
+                <li><a href="./admin.php">Generar reporte</a></li>
+            </ul>
+        </nav>
+        <a class="btn" href="../index.php?info=2"><button>Cerrar sesión</button></a>
+    </header>
     <div class="container-fluid">
         <div class="row">
-            
+
             <div class="col-md-9">
                 <div class="jumbotron">
-                    <h2>
+                    <h2 style="color: black;">
                         Ciudadanos Registrados
                     </h2>
-                    <table class="table table-dark table-striped">
+                    <table class="table col-sm-6" class="table table-dark table-striped">
                         <thead>
                             <tr>
                                 <th scope="col">identificacion</th>
@@ -54,23 +64,23 @@ require_once(CONTROLLER_PATH ."ciudadanos.php");
                             while ($row = mysqli_fetch_assoc($result)) {
 
                             ?>
-                            <tr>
-                                <td>
-                                    <?php echo $row["identificacion"]; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row["nombres"]; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row["apellidos"]; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row["fx_nacimiento"]; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row["lugar_nacimiento"]; ?>
-                                </td>
-                            </tr>
+                                <tr>
+                                    <td>
+                                        <?php echo $row["identificacion"]; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $row["nombres"]; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $row["apellidos"]; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $row["fx_nacimiento"]; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $row["lugar_nacimiento"]; ?>
+                                    </td>
+                                </tr>
                             <?php
                             }
                             ?>
