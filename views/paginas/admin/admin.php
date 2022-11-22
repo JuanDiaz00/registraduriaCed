73703d926f448dfa18292cd6f0df7d7f24cc5f70
JuanDiaz <juane.diazt@konradlorenz.edu.co>
2022-11-22 17:34:57 -0500

    Añadido head al HTML
    N/A

--- views/paginas/admin/admin.php
+++ views/paginas/admin/admin.php
@@ -1,1 +1,29 @@
-<html></html>
+<?php
+define('LIBRARIES_PATH', '../../../modelo/');
+define('CONTROLLER_PATH', '../../../controller/');
+define('VIEWS_PATH', '../../../views/');
+define('CSS_PATH', '../../estilos/');
+define('JS_PATH', '../../scripts/');
+
+if (!defined('CONFIG_PATH')) {
+    define('CONFIG_PATH', '../../../config/');
+}
+
+require_once(CONTROLLER_PATH . "ciudadanos.php");
+?>
+<html>
+<head>
+    <title>Ingreso</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="../estilos/styles.css">
+
+    </head>
+
+    
+</html>
