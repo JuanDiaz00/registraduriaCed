568a85d1bcb1de2e58335287fd5056ecac746130
NicolasFS05 <nicolas.fonsecas@konradlorenz.edu.co>
2022-11-23 21:15:22 -0500

    Ay! Cristian...
    N/A

--- views/paginas/user/mostrar_ciudadanos.php
+++ views/paginas/user/mostrar_ciudadanos.php
@@ -20,7 +20,7 @@ require_once(CONTROLLER_PATH . "ciudadanos.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../estilos/bootstrap.min.css">
-    <link rel="stylesheet" href="../../estilos/registrar_ciudadano.css">
+    <link rel="stylesheet" href="../../estilos/user.css">
     <link rel="stylesheet" href="../../estilos/menu.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <title>Administración ciudadano</title>
@@ -35,9 +35,7 @@ require_once(CONTROLLER_PATH . "ciudadanos.php");
             </div>
             <nav>
                 <ul class="nav-links">
-                    <li><a href="/views/paginas/admin/registrar_ciudadano.php">Ver ciudadanos</a></li>
-
-                    <li><a href="./admin.php">Generar reporte</a></li>
+                    <li><a href="#">Ver ciudadanos</a></li>
                 </ul>
             </nav>
             <a class="btn" href="../index.php?info=2"><button>Cerrar sesión</button></a>
@@ -68,34 +66,34 @@ require_once(CONTROLLER_PATH . "ciudadanos.php");
                                     $result = getAllCiudadanos();
                                     while ($row = mysqli_fetch_assoc($result)) {
                                     ?>
-                                    <tr>
-                                        <td>
-                                            <?php echo $row["identificacion"]; ?>
-                                        </td>
-                                        <td>
-                                            <?php echo $row["nombres"]; ?>
-                                        </td>
-                                        <td>
-                                            <?php echo $row["apellidos"]; ?>
-                                        </td>
-                                        <td>
-                                            <?php echo $row["fx_nacimiento"]; ?>
-                                        </td>
-                                        <td>
-                                            <?php echo $row["lugar_nacimiento"]; ?>
-                                        </td>
-                                        <td>
-                                            <a href="?id=<?php echo $row["id"]; ?>">
-                                                <i class="fa-solid fa-pen-to-square"></i>
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <a href="?id=<?php echo $row["id"]; ?>&elimina=1">
-                                                <i class="fa fa-trash" aria-hidden="true"></i>
-                                            </a>
-                                        </td>
+                                        <tr>
+                                            <td>
+                                                <?php echo $row["identificacion"]; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $row["nombres"]; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $row["apellidos"]; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $row["fx_nacimiento"]; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $row["lugar_nacimiento"]; ?>
+                                            </td>
+                                            <td>
+                                                <a href="?id=<?php echo $row["id"]; ?>">
+                                                    <i class="fa-solid fa-pen-to-square"></i>
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <a href="?id=<?php echo $row["id"]; ?>&elimina=1">
+                                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                                                </a>
+                                            </td>
 
-                                    </tr>
+                                        </tr>
                                     <?php
                                     }
                                     ?>
@@ -115,72 +113,72 @@ require_once(CONTROLLER_PATH . "ciudadanos.php");
             while ($row = mysqli_fetch_assoc($result)) {
                 //print_r($row);
         ?>
-        <div class="box2">
-            <div class="box">
+                <div class="box2">
+                    <div class="box">
 
-                <form action="../../../controller/actualizar.php" method="POST">
-                    <h2>Registrar ciudadano</h2>
-                    <p style="color: #fff; text-align: left; font-size: 15px;">Por favor ingrese
-                        cada uno de los datos del ciudadano a registrar.</p>
-                    <div class="inputBox col-sm-6">
-                        <input type="text" name="nombres" value="<?php echo $row["nombres"]; ?>">
-                        <span>Nombres</span>
-                        <i></i>
+                        <form action="../../../controller/actualizar.php" method="POST">
+                            <h2>Registrar ciudadano</h2>
+                            <p style="color: #fff; text-align: left; font-size: 15px;">Por favor ingrese
+                                cada uno de los datos del ciudadano a registrar.</p>
+                            <div class="inputBox col-sm-6">
+                                <input type="text" name="nombres" value="<?php echo $row["nombres"]; ?>">
+                                <span>Nombres</span>
+                                <i></i>
+                            </div>
+                            <div class="inputBox col-sm-6">
+                                <input type="text" name="apellidos" value="<?php echo $row["apellidos"]; ?>">
+                                <span>Apellidos</span>
+                                <i></i>
+                            </div>
+                            <div class="inputBox ">
+                                <input type="number" name="identificacion" value="<?php echo $row["identificacion"]; ?>">
+                                <span>Documento de identificación</span>
+                                <i></i>
+                            </div>
+                            <div class="inputBox ">
+                                <input type="text" name="lugar_exp" value="<?php echo $row["lugar_exp"]; ?>">
+                                <span>Lugar de expedición</span>
+                                <i></i>
+                            </div>
+                            <div class="inputBox ">
+                                <input type="date" name="fx_nacimiento" value="<?php echo $row["fx_nacimiento"]; ?>">
+                                <span>Fecha Nacimiento</span>
+                                <i></i>
+                            </div>
+                            <div class="inputBox ">
+                                <input type="text" name="lugar_nacimiento" value="<?php echo $row["estatura"]; ?>">
+                                <span>Lugar Nacimiento</span>
+                                <i></i>
+                            </div>
+                            <div class="inputBox">
+                                <input type="number" name="estatura" value="<?php echo $row["estatura"]; ?>">
+                                <span>Estatura</span>
+                                <i></i>
+                            </div>
+                            <div class="inputBox">
+                                <input type="text" name="gs" value="<?php echo $row["rh"]; ?>">
+                                <span>Grupos Sanguineo</span>
+                                <i></i>
+                            </div>
+                            <div class="inputBox">
+                                <input type="text" name="rh" value="<?php echo $row["gs"]; ?>">
+                                <span>RH</span>
+                                <i></i>
+                            </div>
+                            <br>
+                            <div class="col-12">
+                                <input type="hidden" name=id value="<?php echo $row["id"] ?>">
+                                <input type="hidden" name="actualizaCiudadano">
+                                <button type="submit" class="btn btn-primary">Actualizar</button>
+                            </div>
+                            <input type="hidden" name="New_citizen">
+                        </form>
                     </div>
-                    <div class="inputBox col-sm-6">
-                        <input type="text" name="apellidos" value="<?php echo $row["apellidos"]; ?>">
-                        <span>Apellidos</span>
-                        <i></i>
-                    </div>
-                    <div class="inputBox ">
-                        <input type="number" name="identificacion" value="<?php echo $row["identificacion"]; ?>">
-                        <span>Documento de identificación</span>
-                        <i></i>
-                    </div>
-                    <div class="inputBox ">
-                        <input type="text" name="lugar_exp" value="<?php echo $row["lugar_exp"]; ?>">
-                        <span>Lugar de expedición</span>
-                        <i></i>
-                    </div>
-                    <div class="inputBox ">
-                        <input type="date" name="fx_nacimiento" value="<?php echo $row["fx_nacimiento"]; ?>">
-                        <span>Fecha Nacimiento</span>
-                        <i></i>
-                    </div>
-                    <div class="inputBox ">
-                        <input type="text" name="lugar_nacimiento" value="<?php echo $row["estatura"]; ?>">
-                        <span>Lugar Nacimiento</span>
-                        <i></i>
-                    </div>
-                    <div class="inputBox">
-                        <input type="number" name="estatura" value="<?php echo $row["estatura"]; ?>">
-                        <span>Estatura</span>
-                        <i></i>
-                    </div>
-                    <div class="inputBox">
-                        <input type="text" name="gs" value="<?php echo $row["rh"]; ?>">
-                        <span>Grupos Sanguineo</span>
-                        <i></i>
-                    </div>
-                    <div class="inputBox">
-                        <input type="text" name="rh" value="<?php echo $row["gs"]; ?>">
-                        <span>RH</span>
-                        <i></i>
-                    </div>
-                    <br>
-                    <div class="col-12">
-                        <input type="hidden" name=id value="<?php echo $row["id"] ?>">
-                        <input type="hidden" name="actualizaCiudadano">
-                        <button type="submit" class="btn btn-primary">Actualizar</button>
-                    </div>
-                    <input type="hidden" name="New_citizen">
-                </form>
-            </div>
-        </div>
+                </div>
         <?php
             }
         }
-                            ?>
+        ?>
         </div>
     </body>
 
